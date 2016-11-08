3b18b221c9b3d745c3fe431e56cf2bd8ed73266c
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2016-11-08 06:19:27 -0600

    implementando referesncias inversas

--- src/frauducaca.c
+++ src/frauducaca.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
 #include<limits.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <sys/time.h>
+#include <math.h>
+
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#else
+#include <time.h>
+#include <stdarg.h>
+#endif
+
+typedef char tipo_dato;
+typedef unsigned int natural;
+typedef long long entero_largo;
+typedef unsigned long long bitch_vector;
+
+typedef enum BOOLEANOS {
+	falso = 0, verdadero
+} bool;
 
 #define MAX_NUMEROS 200000
 #define MAX_VALOR 200
@@ -12,29 +34,124 @@
 
 #define FRAUDUCACA_BUF_STATICO_DUMP_ARBOL (char[1000] ) { '\0' }
 
-//#define FRAUDUCACA_VALIDAR_ARBOLINES
+#define FRAUDUCACA_VALIDAR_ARBOLINES
 
+#ifdef FRAUDUCACA_VALIDAR_ARBOLINES
+#define CACA_COMUN_LOG
+#endif
+
+#ifdef CACA_COMUN_LOG
+#include <execinfo.h>
+#endif
+
+#if 1
+#define CACA_LOG_MAX_TAM_CADENA 200
+#define CACA_COMUN_ASSERT_DUROTE 0
+#define CACA_COMUN_ASSERT_SUAVECITO 1
+#define CACA_COMUN_ASSERT_NIMADRES 2
 /*
- #define caca_log_debug printf
+ #define CACA_COMUN_TIPO_ASSERT CACA_COMUN_ASSERT_SUAVECITO
  */
-#define caca_log_debug(formato, args...) 0
+#define CACA_COMUN_TIPO_ASSERT CACA_COMUN_ASSERT_DUROTE
+
+#define assert_timeout_dummy(condition) 0;
+
+#if CACA_COMUN_TIPO_ASSERT == CACA_COMUN_ASSERT_DUROTE
 #define assert_timeout(condition) assert(condition);
-/*
- #define assert_timeout(condition) 0
- #define assert_timeout(condition) if(!(condition)){printf("fuck\n");sleep(10);assert(condition);}
- */
+#endif
+#if CACA_COMUN_TIPO_ASSERT == CACA_COMUN_ASSERT_SUAVECITO
+#define assert_timeout(condition) if(!(condition)){printf("fuck\n");sleep(10);}
+#endif
+#if CACA_COMUN_TIPO_ASSERT == CACA_COMUN_ASSERT_NIMADRES
+#define assert_timeout(condition) 0
+#endif
 
-typedef char tipo_dato;
-typedef unsigned int natural;
-typedef long long entero_largo;
-typedef unsigned long long bitch_vector;
+#ifdef CACA_COMUN_LOG
+#define caca_log_debug(formato, args...) \
+		do \
+		{ \
+			size_t profundidad = 0; \
+			void *array[CACA_LOG_MAX_TAM_CADENA]; \
+ 			profundidad = backtrace(array, CACA_LOG_MAX_TAM_CADENA); \
+			caca_log_debug_func(formato,__FILE__, __func__, __LINE__,profundidad,##args); \
+		} \
+		while(0);
+#else
+#define caca_log_debug(formato, args...) 0
+#endif
 
-typedef enum BOOLEANOS {
-	falso = 0, verdadero
-} bool;
+#define caca_comun_max(x,y) ((x) < (y) ? (y) : (x))
+#define caca_comun_min(x,y) ((x) < (y) ? (x) : (y))
 
-#define max(x,y) ((x) < (y) ? (y) : (x))
-#define min(x,y) ((x) < (y) ? (x) : (y))
+void caca_comun_current_utc_time(struct timespec *ts) {
+
+#ifdef __MACH__
+	clock_serv_t cclock;
+	mach_timespec_t mts;
+	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+	clock_get_time(cclock, &mts);
+	mach_port_deallocate(mach_task_self(), cclock);
+	ts->tv_sec = mts.tv_sec;
+	ts->tv_nsec = mts.tv_nsec;
+#else
+#ifndef ONLINE_JUDGE
+	clock_gettime(CLOCK_REALTIME, ts);
+#endif
+#endif
+
+}
+
+void caca_comun_timestamp(char *stime) {
+	time_t ltime;
+	struct tm result;
+	long ms;
+	struct timespec spec;
+	char parte_milisecundos[50];
+
+	ltime = time(NULL);
+
+#ifndef ONLINE_JUDGE
+	localtime_r(&ltime, &result);
+	asctime_r(&result, stime);
+#endif
+
+	*(stime + strlen(stime) - 1) = ' ';
+
+	caca_comun_current_utc_time(&spec);
+#ifndef ONLINE_JUDGE
+	ms = round(spec.tv_nsec / 1.0e3);
+#endif
+	sprintf(parte_milisecundos, "%ld", ms);
+	strcat(stime, parte_milisecundos);
+}
+void caca_log_debug_func(const char *format, ...) {
+
+	va_list arg;
+	va_list arg2;
+	const char *PEDAZO_TIMESTAMP_HEADER = "tiempo: %s; ";
+	const char *HEADER =
+			"archivo: %s; funcion: %s; linea %d; nivel: %zd caca 8====D ";
+	char formato[CACA_LOG_MAX_TAM_CADENA + sizeof(HEADER)
+			+ sizeof(PEDAZO_TIMESTAMP_HEADER)] = { '\0' };
+	char pedazo_timestamp[sizeof(PEDAZO_TIMESTAMP_HEADER) + 100] = { '\0' };
+	char cadena_timestamp[100] = { '\0' };
+
+	caca_comun_timestamp(cadena_timestamp);
+	sprintf(pedazo_timestamp, PEDAZO_TIMESTAMP_HEADER, cadena_timestamp);
+
+	strcpy(formato, pedazo_timestamp);
+	strcat(formato, HEADER);
+	strcat(formato, format);
+	strcat(formato, "\n");
+
+	va_start(arg, format);
+	va_copy(arg2, arg);
+	vprintf(formato, arg2);
+	va_end(arg2);
+	va_end(arg);
+}
+
+#endif
 
 /*Declaring heap globally so that we do not need to pass it as an argument every time*/
 
@@ -304,49 +421,87 @@ typedef struct heap_shit {
 /*Initialize Heap*/
 void heap_shit_init(heap_shit *heap, bool es_min) {
 	heap->heap_size = 0;
-	heap->min=es_min;
+	heap->min = es_min;
 	heap->heap[0] = FRAUDUCACA_VALOR_INVALIDO;
-	heap->tablin_idx_pos_en_heap=kh_init_caca()
+	heap->tablin_idx_pos_en_heap = kh_init_caca();
 }
 
 /*Insert an element into the heap */
 void heap_shit_insert(heap_shit *heap_ctx, tipo_dato element) {
 	natural heap_size = ++heap_ctx->heap_size;
 	int ret = 0;
-	khiter_t iter=0;
-	tipo_dato *heap=NULL;
+	khiter_t iter = 0;
+	tipo_dato *heap = NULL;
 
 	heap = heap_ctx->heap;
 
 	heap[heap_size] = element; /*Insert in the last place*/
 	/*Adjust its position*/
 	natural now = heap_size;
-	while (((heap_ctx->min && (int)heap[now / 2] > (int)element) || (!heap_ctx->min && (natural)heap[now / 2] < (natural)element))) {
+	while (((heap_ctx->min && (int) heap[now / 2] > (int) element)
+			|| (!heap_ctx->min && (natural) heap[now / 2] < (natural) element))) {
 //printf("caca now %u de heap %u elem %u\n",now,heap[now],element);
-		natural idx_pos=0;
-		iter=kh_get_caca(heap_ctx->tablin_idx_pos_en_heap, heap+now/2);
-		idx_pos=kh_val(heap_ctx->tablin_idx_pos_en_heap,iter);
+		natural idx_pos = 0;
+		iter = kh_get_caca(heap_ctx->tablin_idx_pos_en_heap, now / 2);
+		assert_timeout(iter!=kh_end(heap_ctx->tablin_idx_pos_en_heap));
+		idx_pos = kh_val(heap_ctx->tablin_idx_pos_en_heap,iter);
+		heap_ctx->indices_valores[heap[now / 2]][idx_pos] = now;
+		kh_del_caca(heap_ctx->tablin_idx_pos_en_heap, iter);
+		iter = kh_put_caca(heap_ctx->tablin_idx_pos_en_heap, now, &ret);
+		assert_timeout(ret);
+		kh_value(heap_ctx->tablin_idx_pos_en_heap, iter)= idx_pos;
 
 		heap[now] = heap[now / 2];
-		heap_ctx->indices_valores[heap[now]][idx_pos]=now;
 		now /= 2;
 	}
 //printf("raise now %u con heap %u y elem %u res %u\n",now,heap[now / 2],element, (unsigned int)heap[now / 2]>(unsigned int)element);	
 
-	iter= kh_put_caca(heap_ctx->tablin_idx_pos_en_heap, heap+now, &ret);
+	iter = kh_put_caca(heap_ctx->tablin_idx_pos_en_heap, now, &ret);
+	assert_timeout(ret);
 	kh_value(heap_ctx->tablin_idx_pos_en_heap, iter)= heap_ctx->num_indices_valores[element];
 	heap[now] = element;
-	heap_ctx->indices_valores[element][heap_ctx->num_indices_valores[element]++]=now;
-	
+	heap_ctx->indices_valores[element][heap_ctx->num_indices_valores[element]++] =
+			now;
+
 //tablin_idx_pos_en_heap
 }
 
 tipo_dato heap_shit_delete(heap_shit *heap_ctx) {
 	natural heap_size = heap_ctx->heap_size;
+	int ret = 0;
+	natural idx_pos = 0;
+	natural idx_pos_las_elem_ocur = 0;
+	khiter_t iter = 0;
 	natural child, now;
 	tipo_dato minElement, lastElement;
 	tipo_dato *heap;
 	heap = heap_ctx->heap;
+
+	iter = kh_get_caca(heap_ctx->tablin_idx_pos_en_heap, heap_size);
+	assert_timeout(iter!=kh_end(heap_ctx->tablin_idx_pos_en_heap));
+	idx_pos_las_elem_ocur = kh_val(heap_ctx->tablin_idx_pos_en_heap,iter);
+	kh_del_caca(heap_ctx->tablin_idx_pos_en_heap, iter);
+
+	iter = kh_get_caca(heap_ctx->tablin_idx_pos_en_heap, 1);
+	assert_timeout(iter!=kh_end(heap_ctx->tablin_idx_pos_en_heap));
+	idx_pos = kh_val(heap_ctx->tablin_idx_pos_en_heap,iter);
+	kh_del_caca(heap_ctx->tablin_idx_pos_en_heap, iter);
+	if (heap_ctx->num_indices_valores[heap[1]] > 1) {
+		natural idx_ocurrencia_ultimo =
+				heap_ctx->indices_valores[heap[1]][--heap_ctx->num_indices_valores[heap[1]]];
+		heap_ctx->indices_valores[heap[1]][idx_pos] = idx_ocurrencia_ultimo;
+		iter = kh_get_caca(heap_ctx->tablin_idx_pos_en_heap,
+				idx_ocurrencia_ultimo);
+		assert_timeout(iter!=kh_end(heap_ctx->tablin_idx_pos_en_heap));
+		kh_val(heap_ctx->tablin_idx_pos_en_heap,iter)=idx_pos;
+		heap_ctx->indices_valores[heap[1]][heap_ctx->num_indices_valores[heap[1]]
+				+ 1] = 0;
+
+	} else {
+		heap_ctx->indices_valores[heap[1]][--heap_ctx->num_indices_valores[heap[1]]] =
+				0;
+	}
+
 	/* heap[1] is the minimum element. So we remove heap[1]. Size of the heap is decreased.
 	 Now heap[1] has to be filled. We put the last element in its place and see if it fits.
 	 If it does not fit, take minimum element among both its children and replaces parent with it.
@@ -360,19 +515,35 @@ tipo_dato heap_shit_delete(heap_shit *heap_ctx) {
 		child = now * 2;
 		/*child!=heap_size beacuse heap[heap_size+1] does not exist, which means it has only one
 		 child */
-		if (child != heap_size && ((heap_ctx->min && heap[child + 1] < heap[child]) || (!heap_ctx->min && heap[child + 1] > heap[child]))) {
+		if (child != heap_size
+				&& ((heap_ctx->min && heap[child + 1] < heap[child])
+						|| (!heap_ctx->min && heap[child + 1] > heap[child]))) {
 			child++;
 		}
 		/* To check if the last element fits ot not it suffices to check if the last element
 		 is less than the minimum element among both the children*/
 //printf("last %u heap %u\n",lastElement,heap[child]);
-		if ((heap_ctx->min && lastElement > heap[child]) || (!heap_ctx->min && lastElement < heap[child])) {
+		if ((heap_ctx->min && lastElement > heap[child])
+				|| (!heap_ctx->min && lastElement < heap[child])) {
+			iter = kh_get_caca(heap_ctx->tablin_idx_pos_en_heap, child);
+			assert_timeout(iter!=kh_end(heap_ctx->tablin_idx_pos_en_heap));
+			idx_pos = kh_val(heap_ctx->tablin_idx_pos_en_heap,iter);
+			heap_ctx->indices_valores[heap[child]][idx_pos] = now;
+			kh_del_caca(heap_ctx->tablin_idx_pos_en_heap, iter);
+			iter = kh_put_caca(heap_ctx->tablin_idx_pos_en_heap, now, &ret);
+			assert_timeout(ret);
+			kh_value(heap_ctx->tablin_idx_pos_en_heap, iter)= idx_pos;
+
 			heap[now] = heap[child];
 		} else /* It fits there */
 		{
 			break;
 		}
 	}
+	iter = kh_put_caca(heap_ctx->tablin_idx_pos_en_heap, now, &ret);
+	assert_timeout(ret);
+	kh_val(heap_ctx->tablin_idx_pos_en_heap,iter)=idx_pos_las_elem_ocur;
+	heap_ctx->indices_valores[lastElement][idx_pos_las_elem_ocur] = now;
 	heap[now] = lastElement;
 	heap_ctx->heap_size = heap_size;
 	return minElement;
@@ -381,26 +552,23 @@ tipo_dato heap_shit_delete(heap_shit *heap_ctx) {
 void heap_shit_dumpear(heap_shit *heap_ctx) {
 	natural heap_size = heap_ctx->heap_size;
 	for (int i = 0; i <= heap_size; i++) {
-		printf("mierda %u es %u\n", i, heap_ctx->heap[i]);
+		caca_log_debug("mierda %u es %u\n", i, heap_ctx->heap[i]);
 	}
 }
 
 #endif
 int main() {
-	bool es_min=verdadero;
+	bool es_min = verdadero;
 	heap_shit *heap_ctx = &(heap_shit ) { 0 };
-	tipo_dato *caca_prueba=NULL;
+	tipo_dato *caca_prueba = NULL;
 	int number_of_elements;
 //	scanf("%d", &number_of_elements);
 	int iter, element;
-	heap_shit_init(heap_ctx,es_min);
-	if(!es_min)
-	{
-		caca_prueba = (tipo_dato []){ 0, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	}
-	else
-	{
-		caca_prueba = (tipo_dato[]){ 10,9,8,7,6,5,4,3,2,0};
+	heap_shit_init(heap_ctx, es_min);
+	if (!es_min) {
+		caca_prueba = (tipo_dato[] ) { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	} else {
+		caca_prueba = (tipo_dato[] ) { 10, 9, 8, 7, 6, 5, 4, 3, 2, 0 };
 	}
 	number_of_elements = 10;
 	printf("nume elems %u\n", number_of_elements);
