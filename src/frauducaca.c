e966e493151afeb8d6fe9bbd2434b09555c59ad0
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2016-11-13 20:44:13 -0600

    casteado a char para que detecte -1

--- src/frauducaca.c
+++ src/frauducaca.c
@@ -530,7 +530,7 @@ void heap_shit_insert(heap_shit *heap_ctx, tipo_dato element) {
 	heap[heap_size] = element; /*Insert in the last place*/
 	/*Adjust its position*/
 	natural now = heap_size;
-	while (((heap_ctx->min && (int) heap[now / 2] > (int) element)
+	while (((heap_ctx->min && (char) heap[now / 2] > (char) element)
 			|| (!heap_ctx->min && (natural) heap[now / 2] < (natural) element))) {
 //printf("caca now %u de heap %u elem %u\n",now,heap[now],element);
 		natural idx_pos = 0;
